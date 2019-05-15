1d529c6451b1566fefd251529984f863e0d39c3f
xlj997 <123.@qq.com>
2019-05-15 20:53:11 +0800

    学习视图 使用模板 模板传参

--- a1/booktest/models.py
+++ a1/booktest/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-
+GENDER = (("man",'男'),('women','女'))
 
 class aaa(models.Model):
 
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30,verbose_name='书名')
 
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True,verbose_name='日期')
 
     def __str__(self):
         return self.title
@@ -17,13 +17,15 @@ class aaa(models.Model):
 
 class bbb(models.Model):
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,verbose_name='角色名')
 
-    gender = models.BooleanField(default=True)
+    # gender = models.BooleanField(default=True,verbose_name='性别')
 
-    skill = models.CharField(max_length=50,null=True)
+    gender = models.CharField(max_length=10,choices=GENDER,verbose_name='性别')
 
-    boot_id = models.ForeignKey(aaa,on_delete=models.CASCADE)
+    skill = models.CharField(max_length=50,null=True,verbose_name='绝招')
+
+    boot_id = models.ForeignKey(aaa,on_delete=models.CASCADE,verbose_name='书')
 
     def __str__(self):
         return self.name
--- a1/booktest/views.py
+++ a1/booktest/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import aaa,bbb
 
 # Create your views here.
+
+def index(request):
+
+
+
+    temp = loader.get_template('booktest/shouye.html')
+
+    xur =temp.render({})
+
+    return HttpResponse(xur)
+
+    # return HttpResponse('首页')
+
+
+def list(request):
+
+    aa = aaa.objects.all()
+    # print(aa)
+
+
+    temp = loader.get_template('booktest/list.html')
+
+    xur = temp.render({'aa':aa})
+
+    return HttpResponse(xur)
+
+def xq(request,id):
+    try:
+        bb = aaa.objects.get(pk=id)
+        # print(bb)
+
+    except Exception as e:
+        return HttpResponse('没有这本书')
+
+    temp = loader.get_template('booktest/xiangqing.html')
+
+    xur = temp.render({'bb': bb})
+
+    return HttpResponse(xur)
+
+
+
+
+
+
+
